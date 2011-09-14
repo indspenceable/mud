004c8197955c54983a59da4f94b13ca8512e1c0d
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2011-09-14 12:26:29 -0700

    Fixed welcome message slightly, and logging.

--- lib/unbound_connection.rb
+++ lib/unbound_connection.rb
@@ -10,14 +10,14 @@ class Unbound
     Log.debug "Attempting to login as #{data}"
     if Player.exists?(:name => data)
       @connection.send_data("logged in as #{data}.\n")
-      @connection.send_data("Shortcuts\n")
+      @connection.send_data("Welcome to the mud. There will probably be a welcome message here, in the future.\n")
       @player = Player.find_by_name(data)
       CONNECTIONS[@player.id] = @connection
       @player.room.echo "#{@player.name} appears magically.", @player
       Log.debug "Logged in!"
       @player.id
     else
-      Log.debug("Unable to.")
+      Log.debug("Unable to Log in.")
       @connection.send_data "Unable to log in. Try again.\n"
       nil
     end
